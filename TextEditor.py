f259a64c60d53df49375ac9a2106908c4c808413
22-516 <136758602+22-516@users.noreply.github.com>
2024-05-16 13:56:45 +1200

    new editor class for testing

--- TextEditor.py
+++ TextEditor.py
@@ -1,58 +1,28 @@
-import sys
-
-from PyQt6.QtCore import *
+from PyQt6.QtCore import *  # temp
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
-from GUIController import *  # FileContainer
 
-import random  # testing
-
-
-class MainWindow(QMainWindow):
+class TextEditor(QTextEdit):
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("Editor")
-        self.setGeometry(0, 0, 800, 600)
-
-        self.tabs = QTabWidget()
-        self.tabs.setTabPosition(QTabWidget.TabPosition.North)
-        self.setCentralWidget(self.tabs)
-
-        self.textEditor = QTextEdit()
+        #layout = QVBoxLayout(self)
+        
+        
+        #self.textArea = QTextEdit()
+        
+        for i in range(9):
+            self.append("testing")
+        
+        #layout.addWidget(self.textArea)
+        #print(self.textArea.document())
+        
         
 
+    '''def onClicked(self):
+        print("clicked button", self.labelName)
 
-
-
-
-
-
-        recentTabPage = QScrollArea()
-        recentTabPage.setAutoFillBackground(True)
-        # recentTabPage.setSizeAdjustPolicy()
-        recentTabPage.setWidgetResizable(True)
-
-        scrollingFrame = QFrame()
-        scrollingFrame.setAutoFillBackground(True)
-
-        scrollingVLayout = QVBoxLayout()
-
-        for i in range(10):
-            tab1 = FileContainer(str(random.randrange(0, 1000000)))
-            scrollingVLayout.addWidget(tab1)
-
-        scrollingFrame.setLayout(scrollingVLayout)
-        recentTabPage.setWidget(scrollingFrame)
-
-        self.tabs.addTab(recentTabPage, "Recent")
-        self.setCentralWidget(self.tabs)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
+    def onClicked2(self):
+        print("clicked button 2", self.labelName)
+        self.setParent(None)
+        # self.hide()'''
