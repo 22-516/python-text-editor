e08b9aadb35660d84bcd9af5b1dfc87b9a385270
22-516 <136758602+22-516@users.noreply.github.com>
2024-08-16 12:21:06 +1200

    fixed an issue where setting the font would not change just the font family but rather the entire font, which would reset font size, colour, etc

--- texteditor.py
+++ texteditor.py
@@ -12,7 +12,7 @@ IMAGE_EXTENSIONS = [
 class TextEditor(QTextEdit):
     def __init__(self, file_path=""):
         super().__init__()
-        
+
         self.file_path = file_path      
         _1, file_extension = os.path.splitext(file_path)
         self.file_name = os.path.basename(file_path) or "New Document"
@@ -21,22 +21,22 @@ class TextEditor(QTextEdit):
         randomTestingPageId = str(random.randint(1, 100)) # testing new tabs
         for _ in range(9):
             self.append("testing " + randomTestingPageId)
-            
+
         self.document().setModified(False) # temporary to allow for placeholder text on launch
-            
+
     def canInsertFromMimeData(self, source):
         if source.hasImage():
             return True
         else:
             return super().canInsertFromMimeData(source)
-            #return super(TextEditor, self).canInsertFromMimeData(source)
+            # return super(TextEditor, self).canInsertFromMimeData(source)
 
     def createMimeDataFromSelection(self):
         cursor = self.textCursor()
         if len(cursor.selectedText()) == 1: # if only one character is selected, check if it's an image
             cursor.setPosition(cursor.selectionEnd())
             fmt = cursor.charFormat()
-            #print(fmt)
+            # print(fmt)
             if fmt.isImageFormat(): 
                 fmt = fmt.toImageFormat() # if selection is an image, convert to QTextImageFormat
                 print(fmt.name())
@@ -95,20 +95,20 @@ class TextEditor(QTextEdit):
         self.file_extension = file_extension or None
 
     # text formatting
-    
+
     def font_bold(self):
         font_weight = self.fontWeight()
         return font_weight == QFont.Weight.Bold
-    
+
     def font_size(self):
         # font_size = str(self.fontPointSize()).split('.', maxsplit=1)[0]
         # print(font_size)
         font_size = f"{self.currentFont().pointSizeF():g}"
-        #print(font_size)
+        # print(font_size)
         return font_size
 
     def toggle_selected_bold(self):
-        #self.setFontWeight(new_font_weight := QFont.Weight.Bold if self.fontWeight() == QFont.Weight.Normal else QFont.Weight.Normal)
+        # self.setFontWeight(new_font_weight := QFont.Weight.Bold if self.fontWeight() == QFont.Weight.Normal else QFont.Weight.Normal)
         self.setFontWeight(new_font_weight := QFont.Weight.Bold if not self.font_bold() else QFont.Weight.Normal)
         return new_font_weight
 
@@ -119,20 +119,38 @@ class TextEditor(QTextEdit):
     def toggle_selected_italics(self):
         self.setFontItalic(new_font_italics := not self.fontItalic())
         return new_font_italics
-    
+
     def change_font(self, new_font : QFont):
-        self.setCurrentFont(new_font)
+        cursor = self.textCursor()
         
+        selection_end = cursor.selectionEnd()
+        selection_start = cursor.selectionStart()
+
+        cursor.setPosition(selection_start)
+
+        while cursor.position() < selection_end and cursor.position() >= selection_start:
+            new_format = QTextCharFormat()
+            new_format.setFont(new_font)
+            new_format.setFontPointSize(cursor.charFormat().font().pointSizeF())
+            new_format.setFontWeight(cursor.charFormat().font().weight())
+            new_format.setFontItalic(cursor.charFormat().font().italic())
+            new_format.setFontUnderline(cursor.charFormat().font().underline())
+            new_format.setBackground(cursor.charFormat().background())
+            new_format.setForeground(cursor.charFormat().foreground())
+            cursor.mergeCharFormat(new_format)
+            cursor.movePosition(QTextCursor.MoveOperation.NextCharacter, QTextCursor.MoveMode.MoveAnchor)
+            cursor.movePosition(QTextCursor.MoveOperation.NextCharacter, QTextCursor.MoveMode.KeepAnchor)
+
     def change_highlight(self, new_highlight):
         if self.textBackgroundColor() == new_highlight:
-            #self.setTextBackgroundColor(QColor())
+            # self.setTextBackgroundColor(QColor())
             self.setTextBackgroundColor(QColor().fromRgb(255, 255, 255, 1))
         else:
             self.setTextBackgroundColor(new_highlight)
-        
+
     def change_color(self, new_colour):
         self.setTextColor(new_colour)
-        
+
     def change_font_size(self, new_font_size):
         try:
             font_size = float(new_font_size)
